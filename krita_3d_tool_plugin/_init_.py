db04185133bfac043dcbab76eb4afe0f95c02fd5
Yash <yashkapadiya2012@gmail.com>
2026-04-13 16:53:40 +0530

    Base

---
+++ krita_3d_tool_plugin/_init_.py
@@ -0,0 +1,30 @@
+# plugin/my_docker.py
+from krita import DockWidget, DockWidgetFactory, DockWidgetFactoryBase #type:ignore
+from PyQt5.QtWidgets import QWidget, QVBoxLayout, QLabel #type:ignore
+
+
+class ThreeDToolDocker(DockWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("3D Tool")
+        
+        mainWidget = QWidget(self) # ? Stating that this new Widget is child of This Docker
+        
+        self.setWidget(mainWidget)
+        layout = QVBoxLayout()
+        mainWidget.setLayout(layout)
+        
+        label = QLabel("Awaiting connection...", mainWidget)
+        layout.addWidget(label)
+
+    # Required method by Krita, triggered when switching tabs/canvases
+    def canvasChanged(self, canvas):
+        pass
+
+
+DOCKER_ID = 'krita_3d_tool_docker'
+
+# Register the docker with Krita
+Krita.instance().addDockWidgetFactory( #type:ignore
+    DockWidgetFactory(DOCKER_ID, DockWidgetFactoryBase.DockRight, ThreeDToolDocker) 
+) 
