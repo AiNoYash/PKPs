4c2e8f45c8b907383927093a23331a19267b77de
Yash <yashkapadiya2012@gmail.com>
2026-04-26 22:52:24 +0530

    adding cameras

--- Krita 3D Tool Electron App/src/renderer/src/_classes/ObjectFactory.js
+++ Krita 3D Tool Electron App/src/renderer/src/_classes/ObjectFactory.js
@@ -13,6 +13,17 @@ const generateBaseNode = (name, type, parentId = null) => ({
     transform: { position: { x: 0, y: 0, z: 0 }, rotation: { x: 0, y: 0, z: 0 }, scale: { x: 1, y: 1, z: 1 } }
 });
 
+const generateCamera = (cameraType) => {
+
+
+    return {
+        far: 2000,
+        fov: 50,
+        near: 0.1,
+        manual: false,
+        aspect: null,
+    };
+}
 
 
 export const ObjectFactory = {
@@ -23,6 +34,13 @@ export const ObjectFactory = {
     createGeometricObject: (name = 'New Object', geometryType, parentId = null) => {
         const baseNode = generateBaseNode(name, ObjectTypes.THREE_D, parentId);
         baseNode.meshData = generateMeshData(geometryType);
+        return baseNode;
+    },
+
+    createCameraObject: (name = 'New Camera', cameraType, parentId = null) => {
+        let baseNode = generateBaseNode(name, ObjectTypes.CAMERA, parentId);
+        baseNode.cameraData = generateCamera(cameraType);
+
         return baseNode;
     }
 };
--- Krita 3D Tool Electron App/src/renderer/src/components/menus/HierarchyMenus.js
+++ Krita 3D Tool Electron App/src/renderer/src/components/menus/HierarchyMenus.js
@@ -2,7 +2,8 @@ import { useStore } from "../../context/useStore";
 import { ObjectFactory } from "../../_classes/ObjectFactory";
 import { GeometryTypes } from "../../_enums/GeometryTypesEnum";
 
-const state = useStore.getState();
+// ! Never do this as it will give stale states
+// const state = useStore.getState(); 
 
 export const emptySpaceMenuItems = [
     {
@@ -57,10 +58,60 @@ export const emptySpaceMenuItems = [
 ];
 
 
+
 export const objectMenuItems = [
-    ...emptySpaceMenuItems,
+    {
+        label: "Create Child",
+        children: [
+            {
+                label: "Empty Group",
+                action: () => useStore.getState().addChildObject(useStore.getState().activeMenusObjectId, ObjectFactory.createGroup('New Group'))
+            },
+            { type: "divider" },
+            {
+                label: "3D Object",
+                children: [
+                    { label: "Box", action: () => useStore.getState().addChildObject(useStore.getState().activeMenusObjectId, ObjectFactory.createGeometricObject('Box', GeometryTypes.BOX)) },
+                    { label: "Capsule", action: () => useStore.getState().addChildObject(useStore.getState().activeMenusObjectId, ObjectFactory.createGeometricObject('Capsule', GeometryTypes.CAPSULE)) },
+                    { label: "Cone", action: () => useStore.getState().addChildObject(useStore.getState().activeMenusObjectId, ObjectFactory.createGeometricObject('Cone', GeometryTypes.CONE)) },
+                    { label: "Cylinder", action: () => useStore.getState().addChildObject(useStore.getState().activeMenusObjectId, ObjectFactory.createGeometricObject('Cylinder', GeometryTypes.CYLINDER)) },
+                    { label: "Sphere", action: () => useStore.getState().addChildObject(useStore.getState().activeMenusObjectId, ObjectFactory.createGeometricObject('Sphere', GeometryTypes.SPHERE)) },
+                    { label: "Tetrahedron", action: () => useStore.getState().addChildObject(useStore.getState().activeMenusObjectId, ObjectFactory.createGeometricObject('Tetrahedron', GeometryTypes.TETRAHEDRON)) },
+                    { label: "Torus", action: () => useStore.getState().addChildObject(useStore.getState().activeMenusObjectId, ObjectFactory.createGeometricObject('Torus', GeometryTypes.TORUS)) },
+                    { label: "TorusKnot", action: () => useStore.getState().addChildObject(useStore.getState().activeMenusObjectId, ObjectFactory.createGeometricObject('TorusKnot', GeometryTypes.TORUS_KNOT)) }
+                ]
+            },
+            {
+                label: "2D Object",
+                children: [
+                    { label: "Circle", action: () => useStore.getState().addChildObject(useStore.getState().activeMenusObjectId, ObjectFactory.createGeometricObject('Circle', GeometryTypes.CIRCLE)) },
+                    { label: "Plane", action: () => useStore.getState().addChildObject(useStore.getState().activeMenusObjectId, ObjectFactory.createGeometricObject('Plane', GeometryTypes.PLANE)) },
+                    { label: "Ring", action: () => useStore.getState().addChildObject(useStore.getState().activeMenusObjectId, ObjectFactory.createGeometricObject('Ring', GeometryTypes.RING)) }
+                ]
+            },
+            { type: "divider" },
+            {
+                label: "Camera",
+                children: [
+                    { label: "Perspective", action: () => console.log("Add Perspective Camera") },
+                    { label: "Orthographic", action: () => console.log("Add Orthographic Camera") }
+                ]
+            },
+            {
+                label: "Light",
+                children: [
+                    { label: "Ambient", action: () => console.log("Add Ambient Light") },
+                    { label: "Directional", action: () => console.log("Add Directional Light") },
+                    { label: "Point", action: () => console.log("Add Point Light") }
+                ]
+            },
+            { type: "divider" },
+            { label: "Import Model...", action: () => console.log("Open File Dialog") }
+        ]
+    },
+    { type: "divider" },
     {
         label: "Delete",
-        action: () => { console.log("Open File Dialog") }
+        action: () => useStore.getState().deleteObject(useStore.getState().activeMenusObjectId)
     }
 ];
